b5c2ccd722b582bbf79311158c330abdc670f7c1
Tanveer Alam <107429168+tanveerscode@users.noreply.github.com>
2023-01-25 15:51:58 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,14 @@ import 'firebase/compat/firestore';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyApk7Z98W8jD_H3OvVK9CYm-IWd68epXvc",
-    authDomain: "whatapp-clone-f7458.firebaseapp.com",
-    projectId: "whatapp-clone-f7458",
-    storageBucket: "whatapp-clone-f7458.appspot.com",
-    messagingSenderId: "332550870426",
-    appId: "1:332550870426:web:332099c2a2049e19a39623",
-    measurementId: "G-0WKGT74QSZ"
-  };
+  apiKey: "AIzaSyCB3mbPSknWsVM-AL2h58sqaqQoHvPwAFA",
+  authDomain: "whatsap-web-c93d1.firebaseapp.com",
+  projectId: "whatsap-web-c93d1",
+  storageBucket: "whatsap-web-c93d1.appspot.com",
+  messagingSenderId: "384576880236",
+  appId: "1:384576880236:web:2be608850bb9451cc42249",
+  measurementId: "G-YXY50XVTFF"
+};
   // Use this to initialize the firebase App
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
