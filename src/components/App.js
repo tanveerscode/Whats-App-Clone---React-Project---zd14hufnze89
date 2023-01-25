a320c4789dad982c4c48622a5e4f67bc1f4d482b
Tanveer Alam <107429168+tanveerscode@users.noreply.github.com>
2023-01-25 15:31:09 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,43 @@
-import React from 'react'
+import React ,{useEffect} from 'react'
 import '../styles/App.css';
-const App = () => {
+import Chat from './Chat';
+import Sidebar from './Sidebar';
+// import { useStateValue } from '../StateProvider';
+// import { auth } from '../Firebase';
+import{
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
 
-
-  return (
-    <div id="main">
-    </div>
-  )
-}
-
+function App() {
+  // const [dispatch] = useStateValue();
+  // useEffect(() => {
+  //   auth.onAuthStateChanged(user=>{
+  //     dispatch({
+  //       type:"SET_USER",
+  //       user:user
+  //     })
+  //   })
+  // }, [])
+  
+    return (
+          <Router>
+            <Routes>
+              <div className="App">
+                <div className="app__body">
+                  <Sidebar/>
+                  <Route exact path="/">
+                    <Chat/>
+                  </Route>
+                  <Route path="/room/:roomId">
+                    <Chat/>
+                  </Route> 
+                </div>
+              </div>  
+            </Routes>
+          </Router>
+    );
+  }
 
 export default App;
